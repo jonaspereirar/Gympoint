4242bf9f5f5377feeae97471b0da15844d9c05ed
jonaspereirar <jonasprodrigues@gmail.com>
2019-12-29 21:32:49 +0000

    feat add.: Authentication JWT, and input validations ok

--- src/app.js
+++ src/app.js
@@ -6,8 +6,12 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-
+    this.middlewares();
     this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
   }
 
   routes() {
--- src/database/index.js
+++ src/database/index.js
@@ -7,13 +7,12 @@ import databaseConfig from '../config/database';
 const models = [User];
 
 class Database {
-  contructor() {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
     this.init();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
-
     models.map(model => model.init(this.connection));
   }
 }
